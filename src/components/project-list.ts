5846083e81f635ddbe9628dc6998cb9f4d7f57c6
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-12-30 14:20:12 +0100

    organising files and folders

---
+++ src/components/project-list.ts
@@ -0,0 +1,71 @@
+/// <reference path="base-component.ts" />
+
+namespace App {
+    // ProjectList Class
+    export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
+        assignedProjects: Project[];
+
+        constructor(private type: 'active' | 'finished') {
+            super('project-list', 'app', false, `${type}-projects`);
+            this.assignedProjects = [];
+
+            this.configure();
+            this.renderContent();
+        }
+
+        @autobind
+        dragOverHandler(event: DragEvent) {
+            if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+                event.preventDefault();
+                const listEl = this.element.querySelector('ul')!;
+                listEl.classList.add('droppable');
+            }
+        }
+
+        @autobind
+        dropHandler(event: DragEvent) {
+            const projectId = event.dataTransfer!.getData('text/plain');
+            projectState.moveProject(projectId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
+        }
+
+        @autobind
+        dragLeaveHandler(_: DragEvent) {
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.remove('droppable');
+        }
+
+        configure() {
+            // Add a listener to make sure that the dragOverHandler is actually fired
+            this.element.addEventListener('dragover', this.dragOverHandler);
+            this.element.addEventListener('dragleave', this.dragLeaveHandler);
+            this.element.addEventListener('drop', this.dropHandler);
+
+            projectState.addListener((projects: Project[]) => { // Reach out to projectState and call addListener on it to register a listener function. listeners in the end is just a list of functions which we'll eventually call when something changes.
+                // Before we store the projects and render them we wanna filter them, to know which ones are active and finished respectively
+                const relevantProjects = projects.filter(project => {
+                    if (this.type === 'active') {
+                        return project.status === ProjectStatus.Active;
+                    }
+                    return project.status === ProjectStatus.Finished;
+                });
+
+                this.assignedProjects = relevantProjects; // Once something changes, override the assignedProjects with the new projects because something changed in the state.
+                this.renderProjects();
+            });
+        }
+
+        renderContent() {
+            const listId = `${this.type}-projects-list`;
+            this.element.querySelector('ul')!.id = listId;
+            this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+        }
+
+        private renderProjects() {
+            const listElement = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+            listElement.innerHTML = ''; // We get the rid of all list items and then re-render. This prevents re-rendering of the old items in the list which led to some item being rendered twice
+            for (const projectItem of this.assignedProjects) {
+                new ProjectItem(this.element.querySelector('ul')!.id, projectItem);
+            }
+        }
+    }
+}
---
+++ src/state/project-state.ts
@@ -0,0 +1,53 @@
+namespace App {
+    // Project State Management Class: Takes care of the state of the application, set up listeners in the different parts of the app that might be interested.
+
+    type Listener<T> = (items: T[]) => void // Using a type here because we want to encode a function type with one word. Listener is just a bunch of functions and we execute that when anything changes. Since it's a function we have to declare a return type which is void.
+
+    class State<T> {
+        protected listeners: Listener<T>[] = []; // Set up a subscription pattern that manages a list of listeners(functions) inside of the project state. Functions called whenever something changes
+
+        addListener(listenerFn: Listener<T>) {
+            this.listeners.push(listenerFn);
+        }
+    }
+
+    export class ProjectState extends State<Project> {
+        private projects: Project[] = [];
+        private static instance: ProjectState;
+
+        private constructor() { // Creating a private constructor here guarantees that this is a singleton class.
+            super();
+        }
+
+        static getInstance() { // Creating a new instance of ProjectState
+            if (this.instance) {
+                return this.instance;
+            }
+            this.instance = new ProjectState();
+            return this.instance;
+        }
+
+        addProject(title: string, description: string, numberOfPeople: number) {
+            const newProject = new Project(Math.random().toString(), title, description, numberOfPeople, ProjectStatus.Active)
+            this.projects.push(newProject);
+            this.updateListeners();
+        }
+
+        moveProject(projectId: string, newStatus: ProjectStatus) {
+            const project = this.projects.find(project => project.id === projectId);
+            if (project && project.status !== newStatus) {
+                project.status = newStatus;
+                this.updateListeners();
+            }
+        }
+
+        private updateListeners() {
+            for (const listenerFn of this.listeners) {
+                listenerFn(this.projects.slice()); // Calling slice() makes sure we only return a copy of the array and not the original array. Because arrays are passed by reference in JS
+            }
+        }
+    }
+
+    // Creating a Global Instance of Projects State. By doing this, we can now communicate with the class from anywhere in the app, even though the class is private.
+    export const projectState = ProjectState.getInstance(); // With this we're guaranteed we're only working with the exact same object and we always have only one object of that type in the entire application.
+}
