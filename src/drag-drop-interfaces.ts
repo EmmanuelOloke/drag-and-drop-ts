e08335d751d1c00b666944e8198f2a47342dbc8d
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-12-30 12:28:17 +0100

    working with Namespaces

---
+++ src/drag-drop-interfaces.ts
@@ -0,0 +1,14 @@
+// Declaring a Namespace for Drag and Drop Interfaces
+namespace App {
+    // Drag and Drop Interfaces
+    export interface Draggable {
+        dragStartHandler(event: DragEvent): void;
+        dragEndHandler(event: DragEvent): void;
+    }
+
+    export interface DragTarget {
+        dragOverHandler(event: DragEvent): void;
+        dropHandler(event: DragEvent): void;
+        dragLeaveHandler(event: DragEvent): void;
+    }
+}
---
+++ src/project-model.ts
@@ -0,0 +1,8 @@
+namespace App {
+    // Project Type
+    export enum ProjectStatus { Active, Finished } // Using enum here becuase we have exactly 2 options and we only need identifiers instead of strings
+
+    export class Project { // Using a class to create the Project type here instead of an interface or a custom type because we want to be able to instantiate it.
+        constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) { }
+    }
+}
